11eb7e15180548412374b2a5052dd8ca0359743b
lzy-tony <liuzeyu1016@126.com>
2023-01-06 17:56:01 +0800

    fix: fix ix scan with operators

--- src/ix/ix_scan_test.cpp
+++ src/ix/ix_scan_test.cpp
@@ -39,24 +39,88 @@ int main() {
     // LT_OP, <
     int *x = new int(rand() % s + 1);
     handle -> OpenScan(x, LT_OP);
-    int cnt = 0;
     int pid, sid;
     bool getFlag;
     for (int i = *x - 1; i >= 1; i--) {
         getFlag = handle -> GetPrevRecord(pid, sid);
-        std::cout << "i = " << i << " pid = " << pid << " sid = " << sid << std::endl;
         assert (getFlag == true);
         assert (pid == i + s);
         assert (sid == i + s);
 
         getFlag = handle -> GetPrevRecord(pid, sid);
-        std::cout << "i = " << i << " pid = " << pid << " sid = " << sid << std::endl;
         assert (getFlag == true);
         assert (pid == i);
         assert (sid == i);
     }
     getFlag = handle -> GetPrevRecord(pid, sid);
     assert (getFlag == false);
+
+    *x = 0;
+    bool scanFlag = handle -> OpenScan(x, LT_OP);
+    assert (scanFlag == false);
+
+    // GT_OP >
+    *x = rand() % s + 1;
+    handle -> OpenScan(x, GT_OP);
+    for (int i = *x + 1; i <= s; i++) {
+        getFlag = handle -> GetNextRecord(pid, sid);
+        assert (getFlag == true);
+        assert (pid == i);
+        assert (sid == i);
+
+        getFlag = handle -> GetNextRecord(pid, sid);
+        assert (getFlag == true);
+        assert (pid == i + s);
+        assert (sid == i + s);
+    }
+    getFlag = handle -> GetNextRecord(pid, sid);
+    assert (getFlag == false);
+    
+    *x = s + 1;
+    scanFlag = handle -> OpenScan(x, GT_OP);
+    assert (scanFlag == false);
+
+    // LE_OP, <=
+    *x = rand() % s + 1;
+    handle -> OpenScan(x, LE_OP);
+    for (int i = *x; i >= 1; i--) {
+        getFlag = handle -> GetPrevRecord(pid, sid);
+        assert (getFlag == true);
+        assert (pid == i + s);
+        assert (sid == i + s);
+
+        getFlag = handle -> GetPrevRecord(pid, sid);
+        assert (getFlag == true);
+        assert (pid == i);
+        assert (sid == i);
+    }
+    getFlag = handle -> GetPrevRecord(pid, sid);
+    assert (getFlag == false);
+    
+    *x = 0;
+    scanFlag = handle -> OpenScan(x, LE_OP);
+    assert (scanFlag == false);
+
+    // GE_OP, >=
+    *x = rand() % s + 1;
+    handle -> OpenScan(x, GE_OP);
+    for (int i = *x; i <= s; i++) {
+        getFlag = handle -> GetNextRecord(pid, sid);
+        assert (getFlag == true);
+        assert (pid == i);
+        assert (sid == i);
+
+        getFlag = handle -> GetNextRecord(pid, sid);
+        assert (getFlag == true);
+        assert (pid == i + s);
+        assert (sid == i + s);
+    }
+    getFlag = handle -> GetNextRecord(pid, sid);
+    assert (getFlag == false);
+    
+    *x = s + 1;
+    scanFlag = handle -> OpenScan(x, GE_OP);
+    assert (scanFlag == false);
 
     bool closeFlag = ix -> CloseIndex(fid);
     assert (closeFlag == true);
